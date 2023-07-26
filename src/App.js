e5ad3aeed1a93fddfde85e1e3eec313b55e60449
Naveenkumarbala <naveenbala928@gmail.com>
2023-07-26 12:37:27 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import ArticleListPage from "./pages/ArticleListPage";
+import ArticlePage from "./pages/ArticlePage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import NavBar from "./NavBar";
+// const Fun = () => {
+//     return (
+//         <div>
+//             <h1>Fun</h1>
+//         </div>
+//     );
+// }
+const App = () => {
+    //component
+    return (
+        <div>
+            <BrowserRouter>
+                <NavBar/>
+                <div className="App">
+                    <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/articles" element={<ArticleListPage />} />
+                    <Route path="/articles/:articleid" element={<ArticlePage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                 </Routes>
+                </div>
+        </BrowserRouter>
+        </div>
+    );
 }
-
 export default App;
---
+++ src/pages/ArticleListPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticleListPage = () => {
+  return (
+    <div>
+      <h1>ArticleListPage</h1>
+    </div>
+  )
+}
+
+export default ArticleListPage
